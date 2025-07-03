a12084d9ecb0569fa5c88f3f32aaf42bfb741747
Roxane-rg <roxane.gourvennec@optimize-matter.com>
2025-07-03 14:15:48 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8" />
+  <title>Mon Tracker d'Habitudes</title>
+  <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+  <h1>Bienvenue sur ton tracker d'habitudes</h1>
+  <nav>
+    <a href="create.html">➕ Créer une habitude</a> |
+    <a href="habits.html">📊 Voir mes habitudes</a>
+  </nav>
+</body>
+</html>
